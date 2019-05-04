fce4a2da73b4fb6b1b03a759eb47d38deda8b467
Brightmuk <be.rightmuk@gmail.com>
2019-05-04 12:27:27 +0300

    Generate a component to hold information on my profile-not functional yet

---
+++ src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import{User} from '../user';
+import { Repos } from '../repos';
+import { ProfileService } from '../services/profile.service';
+import{HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
