cf2a7f354385c5cf4c02495b181b77152dbe6eeb
Brightmuk <be.rightmuk@gmail.com>
2019-05-04 13:55:55 +0300

    Add functionality to my landing page to display my github profile

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -7,13 +7,24 @@ import{HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.css']
+  styleUrls: ['./my-profile.component.css'],
+
+  providers:[ProfileService]
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  public username ='Brightmuk';
+    repos:Repos;
+    user:User;
 
-  ngOnInit() {
+    constructor(private profileService:ProfileService,public repositoryService:ProfileService) { }
+
+    ngOnInit() {
+      this.profileService.getProfileInfo(this.username);
+      this.user = this.profileService.user;
+
+      this.repositoryService.getProfileRepo(this.username);
+      this.repos = this.repositoryService.repos;
+    }
+
   }
-
-}
