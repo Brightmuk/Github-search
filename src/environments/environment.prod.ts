ebc12a3a3d641133e4b3b62155d28a1f6028b5eb
Brightmuk <be.rightmuk@gmail.com>
2019-05-06 08:52:41 +0300

    Creates a dist folder for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl:'https://api.github.com/users/',
+  apikey:'?access_token=85a853ea537e86b43bc5a3c5b18f4019fba5ee77',
+  repoapikey:'/repos?access_token=85a853ea537e86b43bc5a3c5b18f4019fba5ee77',
 };
